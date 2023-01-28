b5ab220c9af97fc37469ab5f5749cf6f8c19e541
Wesley Persad <26929215+wesleypersad@users.noreply.github.com>
2023-01-28 19:18:14 +0000

    Chat feature added to app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useAuthContext } from './hooks/useAuthContext';
+import socketIO from 'socket.io-client';
 
 //import the navigation & main function
 import Navbar from "./components/Navbar";
@@ -18,11 +18,13 @@ import Admin from "./pages/Admin";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import NotFound from "./pages/Notfound";
+import ChatPage from './components/ChatPage';
+
+// get the server URL directly from the environment variables
+const { REACT_APP_SERVER_URL } = process.env;
+const socket = socketIO.connect(`${REACT_APP_SERVER_URL}`);
 
 function App() {
-  // get user to see if anyone logged in
-  //const { user } = useAuthContext();
-
   return (
     <div className="App">
         <Router>
@@ -30,7 +32,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/blog" element={<Blog />} /> 
-            <Route path="/chat" element={<Chat />} />
+            <Route path="/chat" element={<Chat socket={socket}/>} />
+            <Route path="/chatpage" element={<ChatPage socket={socket} />}></Route>
             <Route path="/donate" element={<Donate />} />
             <Route path="/booking" element={<Booking />} />
             <Route path="/textsms" element={<Textsms />} />            
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,17 +1,38 @@
 import "../App.css";
-//import { useState } from "react";
-//import useFetch from "../components/useFetch";
-//import { useContext } from "react";
-//import DataContext from "../context/DataContext";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
 
-function Chat() {
-    // from  the data context
-    //const { ??? } = useContext(DataContext);
+function Chat({ socket }) {
+    const { user } = useAuthContext();
+    const navigate = useNavigate();
+    const [userName, setUserName] = useState(`${user.username}`);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.setItem('userName', userName);
+        //sends the username and socket ID to the Node.js server
+        socket.emit('newUser', { userName, socketID: socket.id });
+        navigate('/chatpage');
+    };
+    
+    console.log(socket.id);
 
     return (
-        <div className="chat">
-            <h1>Chat Room</h1>
-        </div>
+        <form className="home__container" onSubmit={handleSubmit}>
+            <h2 className="home__header">Sign in to Open Chat</h2>
+            <label htmlFor="username"></label>
+{/*             <input
+                type="text"
+                minLength={6}
+                name="username"
+                id="username"
+                className="username__input"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+            /> */}
+            <button className="home__cta">SIGN IN</button>
+        </form>
     );
 }
 
